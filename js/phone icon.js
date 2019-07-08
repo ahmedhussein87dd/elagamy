5f9d4463d0c08a231659a33c9c0eb5a954cdc470
ahmedhussein <darkrise222@gmail.com>
2019-07-08 17:40:48 +0200

    Initial commit

---
+++ js/phone icon.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+    
+$(document).ready(function()
+{
+    	/*$("#show").hide(function(){*/
+			
+			
+	    $("#hover").hover(function(){
+                      
+   /*                   
+   $(this).css("color","blue");   
+     $("p").css("color","green");
+	
+       */
+		
+		//$("p").hide();
+		//$(this).hide();
+		
+		  	$("#show").fadeIn(600,function(){
+			
+			
+		
+}); 
+	
+});
+ 	
+}); 
+    
+
+    
